ade1209b195d77ef641a1112fc75f1ddebfb955d
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:36:38 -0400

    Solved: Print X N numbers of times (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Print_X_N_numbers_of_times.cpp
@@ -0,0 +1,15 @@
+/*
+ * Problem: Print X N numbers of times
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void printX(int X, int N) {
+        for(i= X;i=N;i)
+
+    }
+};
