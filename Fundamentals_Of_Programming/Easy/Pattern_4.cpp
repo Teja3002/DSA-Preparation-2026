c05d8d88dda8ee84cb690c7861817a454aec36af
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 15:51:07 -0400

    Solved: Pattern 4 (Easy) - Fundamentals Of Programming

---
+++ Fundamentals_Of_Programming/Easy/Pattern_4.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Pattern 4
+ * Difficulty: Easy
+ * Topic: Fundamentals Of Programming
+ * Platform: TUF+ (takeuforward.org)
+ * Date: 2026-05-16
+ */
+
+class Solution {
+public:
+    void pattern4(int n) {
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<i;
+    }
+    cout<<endl;
+}
+    }
+};
