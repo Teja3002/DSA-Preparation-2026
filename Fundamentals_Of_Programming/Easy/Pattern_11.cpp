1a6379e15a5e035db2ce3ed3a9da265bdd67e7ec
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-16 16:07:41 -0400

    Solved: Pattern 11 (Easy) - Fundamentals Of Programming

--- Fundamentals_Of_Programming/Easy/Pattern_11.cpp
+++ Fundamentals_Of_Programming/Easy/Pattern_11.cpp
@@ -6,7 +6,16 @@
  * Date: 2026-05-16
  */
 
-value = 0;
+class Solution {
+   public:
+    void pattern11(int n) {
+        for (int i = 1; i <= n; i++) {
+            int value;
+
+            if (i % 2 == 1) {
+                value = 1;
+            } else {
+                value = 0;
             }
 
             for (int j = 1; j <= i; j++) {
