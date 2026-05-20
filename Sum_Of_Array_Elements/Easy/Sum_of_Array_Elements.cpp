29da091ca5c297c594bf23fb89832682404226af
Teja3002 <119334520+Teja3002@users.noreply.github.com>
2026-05-20 18:12:42 -0400

    Solved: Sum of Array Elements (Easy) - Sum Of Array Elements

--- Sum_Of_Array_Elements/Easy/Sum_of_Array_Elements.cpp
+++ Sum_Of_Array_Elements/Easy/Sum_of_Array_Elements.cpp
@@ -1,10 +1,14 @@
-class Solution{
 public:
     int sum(int arr[], int n) {
-      int num=0;
-      for(int i=0;i<=n;i++){
-        num+=i;
-      }
+
+        int ans = 0; 
+        
+         for(int i = 0; i < n; i++) {
+            // Add the current element to the sum
+            ans = ans + arr[i]; 
+        }
+        
+        // Return the result
+        return ans;
     }
-      return num;
 };
